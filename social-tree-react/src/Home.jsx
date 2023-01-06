0b68d9652925319b23384eb7484e0514976af60c
Geraldo Pires <geraldopireslacerda771@gmail.com>
2023-01-06 20:46:17 -0300

    first commit

---
+++ social-tree-react/src/Home.jsx
@@ -0,0 +1,32 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './Home.css'
+
+export function Home() {
+  
+
+  return (
+    <div className="Home">
+      <header>
+        <div>
+        <img src="" alt="Foto de perfil" />
+        <a href="https://www.instagram.com/coalapapelaria_">@instagram</a>
+        </div>
+      </header>
+      
+      <main>
+        <a href="">LINK 1</a>
+        <a href="">LINK 2</a>
+        <a href="">LINK 3</a>
+        <a href="">LINK 4</a>
+      </main>
+
+      <footer>
+        <p>redes sociais</p>
+      </footer>
+      
+    </div>
+  )
+}
+
+export default Home
